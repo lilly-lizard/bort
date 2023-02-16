da37e701adaec2a9e6432304a51dd610b01216f6
lilly-lizard <bigbanana29@aol.com>
2023-02-16 16:43:30 +1300

    shader module

--- src/shader.rs
+++ src/shader.rs
@@ -1,6 +1,10 @@
 use crate::{device::Device, memory::ALLOCATION_CALLBACK_NONE};
-use ash::vk;
-use std::{error, fmt, fs, io, sync::Arc};
+use ash::{util::read_spv, vk};
+use std::{
+    error, fmt, fs,
+    io::{self, Cursor},
+    sync::Arc,
+};
 
 pub struct Shader {
     handle: vk::ShaderModule,
@@ -11,10 +15,37 @@ pub struct Shader {
 
 impl Shader {
     pub fn new_from_file(device: Arc<Device>, file_path: &str) -> Result<Self, ShaderError> {
-        let bytes = fs::read(file_path).map_err(|e| ShaderError::IOError {
+        let bytes = fs::read(file_path).map_err(|e| ShaderError::FileRead {
             e,
             path: file_path.to_string(),
         })?;
+        let mut cursor = Cursor::new(bytes);
+
+        Self::new_from_spirv(device, &mut cursor)
+    }
+
+    pub fn new_from_spirv<R: io::Read + io::Seek>(
+        device: Arc<Device>,
+        spirv: &mut R,
+    ) -> Result<Self, ShaderError> {
+        let code = read_spv(spirv).map_err(|e| ShaderError::SpirVDecode(e))?;
+        let create_info = vk::ShaderModuleCreateInfo::builder().code(&code);
+
+        Self::new_from_create_info(device, create_info)
+    }
+
+    pub fn new_from_create_info(
+        device: Arc<Device>,
+        create_info: vk::ShaderModuleCreateInfoBuilder,
+    ) -> Result<Self, ShaderError> {
+        let handle = unsafe {
+            device
+                .inner()
+                .create_shader_module(&create_info, ALLOCATION_CALLBACK_NONE)
+        }
+        .map_err(|e| ShaderError::Creation(e))?;
+
+        Ok(Self { handle, device })
     }
 
     // Getters
@@ -40,15 +71,19 @@ impl Drop for Shader {
 
 #[derive(Debug)]
 pub enum ShaderError {
-    IOError { e: io::Error, path: String },
+    FileRead { e: io::Error, path: String },
+    SpirVDecode(io::Error),
+    Creation(vk::Result),
 }
 
 impl fmt::Display for ShaderError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::IOError { e, path } => {
+            Self::FileRead { e, path } => {
                 write!(f, "failed to read file {} due to: {}", path, e)
             }
+            Self::SpirVDecode(e) => write!(f, "failed to decode spirv: {}", e),
+            Self::Creation(e) => write!(f, "shader module creation failed: {}", e),
         }
     }
 }
@@ -56,7 +91,9 @@ impl fmt::Display for ShaderError {
 impl error::Error for ShaderError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
-            Self::IOError { e, .. } => Some(e),
+            Self::FileRead { e, .. } => Some(e),
+            Self::SpirVDecode(e) => Some(e),
+            Self::Creation(e) => Some(e),
         }
     }
 }
