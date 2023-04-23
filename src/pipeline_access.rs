129a06521c0b023d4d6bdbb43aeebbbd99568625
lilly-lizard <bigbanana29@aol.com>
2023-04-23 16:34:04 +1200

    traits now guarantee send + sync

--- src/pipeline_access.rs
+++ src/pipeline_access.rs
@@ -2,7 +2,7 @@ use crate::{DeviceOwned, PipelineLayout};
 use ash::vk;
 use std::sync::Arc;
 
-pub trait PipelineAccess: DeviceOwned {
+pub trait PipelineAccess: DeviceOwned + Send + Sync {
     fn handle(&self) -> vk::Pipeline;
     fn pipeline_layout(&self) -> &Arc<PipelineLayout>;
 }
