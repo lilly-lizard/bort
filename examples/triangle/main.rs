0a84fe2596c06d0d7baa38dbb614b90f94ea8baf
lilly-lizard <bigbanana29@aol.com>
2023-12-06 11:20:39 +1300

    logging and swapchain recreation works!

--- examples/triangle/main.rs
+++ examples/triangle/main.rs
@@ -1,12 +1,13 @@
 use ash::{prelude::VkResult, vk};
 use bort_vk::{
     choose_composite_alpha, is_format_srgb, ApiVersion, ColorBlendState, CommandBuffer,
-    CommandPool, CommandPoolProperties, Device, DynamicState, Fence, Framebuffer,
+    CommandPool, CommandPoolProperties, Device, DeviceOwned, DynamicState, Fence, Framebuffer,
     FramebufferProperties, GraphicsPipeline, GraphicsPipelineProperties, ImageView,
     ImageViewAccess, Instance, PhysicalDevice, PipelineLayout, PipelineLayoutProperties, Queue,
-    RenderPass, Semaphore, ShaderModule, ShaderStage, Subpass, Surface, Swapchain,
+    RenderPass, Semaphore, ShaderModule, ShaderStage, Subpass, Surface, Swapchain, SwapchainImage,
     SwapchainProperties, ViewportState,
 };
+use env_logger::Env;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
@@ -22,7 +23,7 @@ const TITLE: &str = "Triangle (bort example)";
 const DEFAULT_WINDOW_SIZE: [u32; 2] = [700, 500];
 const API_VERSION: ApiVersion = ApiVersion { major: 1, minor: 2 };
 const MAX_FRAMES_IN_FLIGHT: usize = 2;
-const FENCE_TIMEOUT: u64 = 0;
+const FENCE_TIMEOUT: u64 = 1_000_000_000;
 
 #[cfg(not(any(target_os = "macos", target_os = "ios")))]
 pub fn create_entry() -> Result<Arc<ash::Entry>, ash::LoadingError> {
@@ -37,6 +38,10 @@ pub fn create_entry() -> Result<Arc<ash::Entry>, ash::LoadingError> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let log_env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "debug")
+        .write_style_or("MY_LOG_STYLE", "always");
+    env_logger::init_from_env(log_env);
     info!("starting triangle example...");
 
     let event_loop = EventLoop::new()?;
@@ -50,7 +55,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let window = window_builder.build(&event_loop)?;
     info!("created window");
 
-    let mut engine = TriangleExample::new(&window)?;
+    let mut engine = TriangleExample::new(window)?;
 
     event_loop.run(move |event, elwt| {
         if let Event::WindowEvent { event, .. } = event {
@@ -65,10 +70,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                         },
                     ..
                 } => elwt.exit(),
-                WindowEvent::RedrawRequested => {
-                    engine.draw_frame().unwrap();
-                }
-                _ => (),
+                _ => engine.draw_frame().unwrap(),
             }
         }
     })?;
@@ -77,8 +79,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 struct TriangleExample {
-    queue: Arc<Queue>,
+    window: Arc<Window>,
+    surface: Arc<Surface>,
     swapchain: Arc<Swapchain>,
+
+    queue: Arc<Queue>,
     pipeline: GraphicsPipeline,
     render_pass: Arc<RenderPass>,
 
@@ -94,7 +99,7 @@ struct TriangleExample {
 }
 
 impl TriangleExample {
-    pub fn new(window: &Window) -> Result<Self, Box<dyn Error>> {
+    pub fn new(window: Window) -> Result<Self, Box<dyn Error>> {
         let display_handle = window.display_handle()?;
         let window_handle = window.window_handle()?;
 
@@ -169,72 +174,20 @@ impl TriangleExample {
         let queue = Arc::new(Queue::new(device.clone(), queue_family_index as u32, 0)?);
         info!("created queue");
 
-        let surface_capabilities =
-            surface.get_physical_device_surface_capabilities(&physical_device)?;
-        let preferred_swapchain_image_count = surface_capabilities.min_image_count + 1;
-        let surface_format = surface.get_physical_device_surface_formats(&physical_device)?[0];
-        let composite_alpha = choose_composite_alpha(surface_capabilities);
-        let swapchain_properties = SwapchainProperties::new_default(
-            &device,
-            &surface,
-            preferred_swapchain_image_count,
-            surface_format,
-            composite_alpha,
-            vk::ImageUsageFlags::COLOR_ATTACHMENT,
-            window.inner_size().into(),
-        )?;
+        let swapchain_properties = swapchain_properties(&surface, &device, &window)?;
+        let surface_format = swapchain_properties.surface_format;
         let swapchain = Arc::new(Swapchain::new(
             device.clone(),
             surface.clone(),
             swapchain_properties,
         )?);
-        let shaders_write_linear_color =
+        let shaders_should_write_linear_color =
             is_format_srgb(swapchain.properties().surface_format.format);
         info!("created swapchain");
 
-        let swapchain_image_views = swapchain
-            .swapchain_images()
-            .iter()
-            .map(|swapchain_image| {
-                let image_view =
-                    ImageView::new(swapchain_image.clone(), swapchain.image_view_properties())?;
-                Ok(Arc::new(image_view))
-            })
-            .collect::<VkResult<Vec<_>>>()?;
-        info!(
-            "created {} swapchain image views",
-            swapchain_image_views.len()
-        );
+        let swapchain_image_views = create_swapchain_image_views(&swapchain)?;
 
-        let swapchain_attachment_description = vk::AttachmentDescription::builder()
-            .format(surface_format.format)
-            .samples(vk::SampleCountFlags::TYPE_1)
-            .load_op(vk::AttachmentLoadOp::CLEAR)
-            .store_op(vk::AttachmentStoreOp::STORE)
-            .initial_layout(vk::ImageLayout::UNDEFINED)
-            .final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
-
-        let swapchain_attachemnt_reference = vk::AttachmentReference::builder()
-            .attachment(0)
-            .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
-
-        let subpass = Subpass::new(&[swapchain_attachemnt_reference.build()], None, &[]);
-
-        let image_aquire_subpass_dependency = vk::SubpassDependency::builder()
-            .src_subpass(vk::SUBPASS_EXTERNAL)
-            .dst_subpass(0)
-            .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
-            .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
-            .src_access_mask(vk::AccessFlags::empty())
-            .dst_access_mask(vk::AccessFlags::COLOR_ATTACHMENT_WRITE);
-
-        let render_pass = Arc::new(RenderPass::new(
-            device.clone(),
-            [swapchain_attachment_description.build()],
-            [subpass],
-            [image_aquire_subpass_dependency.build()],
-        )?);
-        info!("created render pass");
+        let render_pass = create_render_pass(device.clone(), surface_format)?;
 
         let pipeline_layout_properties = PipelineLayoutProperties::new(Vec::new(), Vec::new());
         let pipeline_layout = Arc::new(PipelineLayout::new(
@@ -287,21 +240,7 @@ impl TriangleExample {
         )?;
         info!("created graphics pipeline");
 
-        let framebuffers = swapchain_image_views
-            .into_iter()
-            .map(|swapchain_image_view| {
-                let attachments: Vec<Arc<dyn ImageViewAccess>> = vec![swapchain_image_view.clone()];
-
-                let framebuffer_properties = FramebufferProperties::new_default(
-                    attachments,
-                    swapchain_image_view.image().dimensions(),
-                );
-
-                let framebuffer = Framebuffer::new(render_pass.clone(), framebuffer_properties)?;
-                Ok(Arc::new(framebuffer))
-            })
-            .collect::<VkResult<Vec<_>>>()?;
-        info!("created {} framebuffers", framebuffers.len());
+        let framebuffers = create_framebuffers(swapchain_image_views, render_pass.clone())?;
 
         let command_pool_properties = CommandPoolProperties {
             flags: vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER,
@@ -327,8 +266,11 @@ impl TriangleExample {
         let current_frame = 0_usize;
 
         Ok(Self {
-            queue,
+            window: Arc::new(window),
+            surface,
             swapchain,
+
+            queue,
             pipeline,
             render_pass,
 
@@ -342,7 +284,7 @@ impl TriangleExample {
         })
     }
 
-    pub fn draw_frame(&mut self) -> VkResult<()> {
+    pub fn draw_frame(&mut self) -> Result<(), Box<dyn Error>> {
         self.in_flight_fences[self.current_frame].wait(FENCE_TIMEOUT)?;
 
         let aquire_res = self.swapchain.aquire_next_image(
@@ -357,7 +299,7 @@ impl TriangleExample {
                 self.recreate_swapchain()?;
                 return Ok(());
             }
-            Err(e) => return Err(e),
+            Err(e) => return Err(e)?,
         };
 
         self.in_flight_fences[self.current_frame].reset()?;
@@ -398,7 +340,7 @@ impl TriangleExample {
             Err(vk::Result::ERROR_OUT_OF_DATE_KHR) | Err(vk::Result::SUBOPTIMAL_KHR) | Ok(true) => {
                 self.recreate_swapchain()?
             }
-            Err(e) => return Err(e),
+            Err(e) => return Err(e)?,
         };
 
         self.current_frame = (self.current_frame + 1) % MAX_FRAMES_IN_FLIGHT;
@@ -439,9 +381,129 @@ impl TriangleExample {
         Ok(())
     }
 
-    pub fn recreate_swapchain(&mut self) -> VkResult<()> {
-        todo!();
+    pub fn recreate_swapchain(&mut self) -> Result<(), Box<dyn Error>> {
+        info!("recreating swapchain...");
+
+        self.queue.device().wait_idle()?;
+        self.framebuffers.clear();
+
+        let swapchain_properties =
+            swapchain_properties(&self.surface, &self.queue.device(), &self.window)?;
+        let surface_format = swapchain_properties.surface_format;
+
+        self.swapchain = self.swapchain.recreate_replace(swapchain_properties)?;
+        let swapchain_image_views = create_swapchain_image_views(&self.swapchain)?;
+
+        self.render_pass = create_render_pass(self.render_pass.device().clone(), surface_format)?;
+        self.framebuffers = create_framebuffers(swapchain_image_views, self.render_pass.clone())?;
+
+        Ok(())
     }
+}
+
+fn swapchain_properties(
+    surface: &Surface,
+    device: &Device,
+    window: &Window,
+) -> Result<SwapchainProperties, Box<dyn Error>> {
+    let surface_capabilities =
+        surface.get_physical_device_surface_capabilities(&device.physical_device())?;
+
+    let preferred_swapchain_image_count = surface_capabilities.min_image_count + 1;
+    let surface_format = surface.get_physical_device_surface_formats(&device.physical_device())?[0];
+    let composite_alpha = choose_composite_alpha(surface_capabilities);
+
+    let swapchain_properties = SwapchainProperties::new_default(
+        device,
+        surface,
+        preferred_swapchain_image_count,
+        surface_format,
+        composite_alpha,
+        vk::ImageUsageFlags::COLOR_ATTACHMENT,
+        window.inner_size().into(),
+    )?;
+
+    Ok(swapchain_properties)
+}
+
+fn create_render_pass(
+    device: Arc<Device>,
+    surface_format: vk::SurfaceFormatKHR,
+) -> Result<Arc<RenderPass>, Box<dyn Error>> {
+    let swapchain_attachment_description = vk::AttachmentDescription::builder()
+        .format(surface_format.format)
+        .samples(vk::SampleCountFlags::TYPE_1)
+        .load_op(vk::AttachmentLoadOp::CLEAR)
+        .store_op(vk::AttachmentStoreOp::STORE)
+        .initial_layout(vk::ImageLayout::UNDEFINED)
+        .final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
+
+    let swapchain_attachemnt_reference = vk::AttachmentReference::builder()
+        .attachment(0)
+        .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
+
+    let subpass = Subpass::new(&[swapchain_attachemnt_reference.build()], None, &[]);
+
+    let image_aquire_subpass_dependency = vk::SubpassDependency::builder()
+        .src_subpass(vk::SUBPASS_EXTERNAL)
+        .dst_subpass(0)
+        .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+        .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+        .src_access_mask(vk::AccessFlags::empty())
+        .dst_access_mask(vk::AccessFlags::COLOR_ATTACHMENT_WRITE);
+
+    let render_pass = Arc::new(RenderPass::new(
+        device,
+        [swapchain_attachment_description.build()],
+        [subpass],
+        [image_aquire_subpass_dependency.build()],
+    )?);
+
+    info!("created render pass");
+    Ok(render_pass)
+}
+
+fn create_swapchain_image_views(
+    swapchain: &Arc<Swapchain>,
+) -> Result<Vec<Arc<ImageView<SwapchainImage>>>, Box<dyn Error>> {
+    let swapchain_image_views = swapchain
+        .swapchain_images()
+        .iter()
+        .map(|swapchain_image| {
+            let image_view =
+                ImageView::new(swapchain_image.clone(), swapchain.image_view_properties())?;
+            Ok(Arc::new(image_view))
+        })
+        .collect::<VkResult<Vec<_>>>()?;
+    info!(
+        "created {} swapchain image views",
+        swapchain_image_views.len()
+    );
+
+    Ok(swapchain_image_views)
+}
+
+fn create_framebuffers(
+    swapchain_image_views: Vec<Arc<ImageView<SwapchainImage>>>,
+    render_pass: Arc<RenderPass>,
+) -> Result<Vec<Arc<Framebuffer>>, Box<dyn Error>> {
+    let framebuffers = swapchain_image_views
+        .into_iter()
+        .map(|swapchain_image_view| {
+            let attachments: Vec<Arc<dyn ImageViewAccess>> = vec![swapchain_image_view.clone()];
+
+            let framebuffer_properties = FramebufferProperties::new_default(
+                attachments,
+                swapchain_image_view.image().dimensions(),
+            );
+
+            let framebuffer = Framebuffer::new(render_pass.clone(), framebuffer_properties)?;
+            Ok(Arc::new(framebuffer))
+        })
+        .collect::<VkResult<Vec<_>>>()?;
+
+    info!("created {} framebuffers", framebuffers.len());
+    Ok(framebuffers)
 }
 
 // ~~ Errors ~~
